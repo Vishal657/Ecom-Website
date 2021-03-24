06b865a646a851d25972a3e47e28a4f35d389909
unknown <vishalpatel17999@gmail.com>
2021-03-24 12:36:48 +0530

    used link or react

--- src/Navigation/NavBar.js
+++ src/Navigation/NavBar.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import Logo from '../Assets/Logo.jpg'
 import "./NavBar.css";
+
 
 const NavBar = (props) => {
     const [enteredText,setEnterdedText] = useState(null)
     return (
         <nav className="navbar navbar-expand-md navbar-light bg-light navigation-bar">
             <div className="container-fluid">
-                <a className="navbar-brand mx-md-auto" href="/">
+                <Link className="navbar-brand mx-md-auto" to="/">
                     <img src={Logo} width="50" height="50" alt="" />
-                </a>
+                </Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
